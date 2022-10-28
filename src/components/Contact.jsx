4c96bf4682b0b238b126effc1fa083184527f7ef
Kenenth Schaefer <kenneth.schaefer0@gmail.com>
2022-10-27 20:25:09 -0500

    Update contact info

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -29,16 +29,14 @@ const Contact = () => {
       <div className="info">
         <h2 className="propertyName">Github:</h2>
         <h2 className="propertyValue">
-          <a href="https://www.github.com/parzidog">
-            https://www.github.com/parzidog
-          </a>
+          <a href="https://www.github.com/parzidog">github.com/parzidog</a>
         </h2>
       </div>
       <div className="info">
         <h2 className="propertyName">Linkedin:</h2>
         <h2 className="propertyValue">
           <a href="https://www.linkedin.com/in/parzidog">
-            https://www.linkedin.com/in/parzidog
+            linkedin.com/in/parzidog
           </a>
         </h2>
       </div>

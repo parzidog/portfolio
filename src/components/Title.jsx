2e15f83be42181ed8d88d4a3a55ac5477ef1d95d
Kenenth Schaefer <kenneth.schaefer0@gmail.com>
2022-11-15 10:37:12 -0600

    Change introduction animations

--- src/components/Title.jsx
+++ src/components/Title.jsx
@@ -4,19 +4,19 @@ import logo from "../assets/logo.svg";
 const Title = () => {
   return (
     <div className="title" id="title">
-      <div className="logo">
+      <div className="logo rotateInRight">
         <img src={logo} alt="" />
       </div>
       <div className="descriptive">
-        <h2 className="descriptor" id="adaptable">
+        <h2 className="descriptor  slideRight" id="adaptable">
           Adaptable
         </h2>
         <div className="line"></div>
-        <h2 className="descriptor" id="creative">
+        <h2 className="descriptor bounceIn" id="creative">
           Creative
         </h2>
         <div className="line"></div>
-        <h2 className="descriptor" id="focused">
+        <h2 className="descriptor slideLeft" id="focused">
           Focused
         </h2>
       </div>

d5ae8f1aba700cea1909980c60b0014f9562886f
Kenny Schaefer <kenneth.schaefer0@gmail.com>
2023-02-09 16:22:57 -0600

    Fix Title styling

--- src/components/Title.jsx
+++ src/components/Title.jsx
@@ -2,21 +2,9 @@ import React, { useEffect } from "react";
 import logo from "../assets/name.svg";
 
 const Title = () => {
-  let isMobile = () => {
-    if (navigator.userAgent.match(/Android/i)
-      || navigator.userAgent.match(/webOS/i)
-      || navigator.userAgent.match(/iPhone/i)
-      || navigator.userAgent.match(/iPad/i)
-      || navigator.userAgent.match(/iPod/i)
-      || navigator.userAgent.match(/BlackBerry/i)
-      || navigator.userAgent.match(/Windows Phone/i)) {
-      mobile = true;
-    }
-  };
-  let mobile = false;
+
   useEffect(() => {
     const title = document.getElementById("title");
-    isMobile();
     title.style.paddingTop = `${window.innerHeight / 2 - 150}px`;
     window.addEventListener("resize", () => {
       title.style.paddingTop = `${window.innerHeight / 2 - 100}px`;
@@ -29,15 +17,15 @@ const Title = () => {
         <img src={logo} alt="" />
       </div>
       <div className="descriptive">
-        <h2 className="descriptor  slideRight" style={!mobile ? { fontSize: 50 } : ""} id="adaptable">
+        <h2 className="descriptor  slideRight" id="adaptable">
           Adaptable
         </h2>
         <div className="line"></div>
-        <h2 className="descriptor bounceIn" style={!mobile ? { fontSize: 50 } : ""} id="creative">
+        <h2 className="descriptor bounceIn" id="creative">
           Creative
         </h2>
         <div className="line"></div>
-        <h2 className="descriptor slideLeft" style={!mobile ? { fontSize: 50 } : ""} id="focused">
+        <h2 className="descriptor slideLeft" id="focused">
           Focused
         </h2>
       </div>

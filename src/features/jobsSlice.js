770a65a48225eab093aa6d026130eb39305c7e08
Kenneth Schaefer <kenneth.schaefer0@gmail.com>
2023-04-14 11:45:26 -0500

    Update Styling

--- src/features/jobsSlice.js
+++ src/features/jobsSlice.js
@@ -4,29 +4,29 @@ import axios from 'axios';
 const initialState = [];
 
 export const fetchJobsAsync = createAsyncThunk('allJobs', async () => {
-    try {
-        const { data } = await axios.get('/api/jobs');
-        return data;
-    } catch (err) {
-        console.log(err);
-    }
+  try {
+    const { data } = await axios.get('/api/jobs');
+    return data;
+  } catch (err) {
+    console.log(err);
+  }
 });
 
 const jobsSlice = createSlice({
-    name: 'jobs',
-    initialState,
-    reducers: {},
-    extraReducers: (builder) => {
-        builder.addCase(fetchJobsAsync.fulfilled, (state, action) => {
-            // Add user to the state array
-            return action.payload;
-        });
-    },
+  name: 'jobs',
+  initialState,
+  reducers: {},
+  extraReducers: (builder) => {
+    builder.addCase(fetchJobsAsync.fulfilled, (state, action) => {
+      // Add user to the state array
+      return action.payload;
+    });
+  },
 });
 
 export const selectJobs = (state) => {
-    console.log(jobsSlice);
-    return state.jobs;
+  console.log(jobsSlice);
+  return state.jobs;
 };
 
 export default jobsSlice.reducer;
